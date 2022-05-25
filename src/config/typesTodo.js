0fd2172fabf54ae36482994584981a9ea320f169
AmirAbasi <amir.abasi.dev@gmail.com>
2022-05-25 15:30:55 +0430

    refactor : ♻ config types todo

---
+++ src/config/typesTodo.js
@@ -0,0 +1,9 @@
+const typesTodo = [
+  { key: 1, name: "Very Easy", color: "#619cfa" },
+  { key: 2, name: "Easy", color: "#125fdb" },
+  { key: 3, name: "Medium", color: "#33db27" },
+  { key: 4, name: "Hard", color: "#e33434" },
+  { key: 5, name: "Very Hard", color: "#e60909" },
+];
+
+export default typesTodo;
