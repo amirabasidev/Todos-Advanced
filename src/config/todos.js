fb82d3d6cc96bedcca98d03cd9662ff80dcc6a07
AmirAbasi <amir.abasi.dev@gmail.com>
2022-05-25 19:12:52 +0430

    refactor : ♻ config todos

---
+++ src/config/todos.js
@@ -0,0 +1,16 @@
+const todos = [
+    {
+      id: 'pzsp3s8hf',
+      name: 'Reading Book JavaScript',
+      type: { name: 'Medium', color: '#33db27' },
+      isComplate: true,
+    },
+    {
+      id: 'heb4nsmhy',
+      name: 'Buy Mobile',
+      type: { name: 'Very Hard', color: '#e60909' },
+      isComplate: false,
+    },
+  ];
+  
+  export default todos;
--- src/context/todos/TodosState.jsx
+++ src/context/todos/TodosState.jsx
@@ -3,16 +3,20 @@ import { useReducer } from "react";
 import todosContext from "./todosContext";
 import todosReducer from "./todosReducer";
 
+import todos from "../../config/todos";
+
 const TodosState = ({ children }) => {
   const initialState = {
-    Todos: [],
+    todos,
   };
 
   const [state, dispatch] = useReducer(todosReducer, initialState);
 
   return (
-    <todosContext.Provider value={{ ...state }}>{children}</todosContext.Provider>
-  )
+    <todosContext.Provider value={{ ...state }}>
+      {children}
+    </todosContext.Provider>
+  );
 };
 
 export default TodosState;
