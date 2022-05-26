530dcad5abe3d0685e9df2d054ca2d77642927ab
AmirAbasi <amir.abasi.dev@gmail.com>
2022-05-26 14:45:20 +0430

    feat : ✨ add component filters to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Layout from "./layout/Layout";
 
 import TodoForm from "./components/todoForm/TodoForm";
 import Todos from "./components/todos/Todos";
+import Filters from "./components/filters/Filters";
 
 const App = () => {
   return (
     <Layout>
       <TodoForm />
+      <Filters />
       <Todos />
     </Layout>
   );
