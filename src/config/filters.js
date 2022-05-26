699c86bd063a1781b155797ba9ec2c5b625c391a
AmirAbasi <amir.abasi.dev@gmail.com>
2022-05-26 12:18:29 +0430

    refactor : ♻ config filters

---
+++ src/config/filters.js
@@ -0,0 +1,10 @@
+export const sortTodos = [
+  { key: 1, title: "A-Z" },
+  { key: 2, title: "Z-A" },
+];
+
+export const statusTodos = [
+  { key: 1, title: "All" },
+  { key: 2, title: "Active" },
+  { key: 3, title: "Completed" },
+];
