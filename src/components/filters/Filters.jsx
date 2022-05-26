af56f56dbf8aad2e725c2cc7d229e68cacd9dcf9
AmirAbasi <amir.abasi.dev@gmail.com>
2022-05-26 13:45:37 +0430

    feat : ✨ create component filters

---
+++ src/components/filters/Filters.jsx
@@ -0,0 +1,15 @@
+import { Form, Row, Card } from "antd";
+
+const Filters = () => {
+  return (
+    <Card>
+      <Form>
+        <Row justify="center" wrap={false} gutter={20}>
+          
+        </Row>
+      </Form>
+    </Card>
+  );
+};
+
+export default Filters;
