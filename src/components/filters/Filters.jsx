9bad872b157800720c9e162939c1ed7c83ccc1e5
AmirAbasi <amir.abasi.dev@gmail.com>
2022-05-26 13:55:05 +0430

    feat : ✨ add types todo to  filters

--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -1,6 +1,7 @@
 import { Form, Row, Card, Col } from "antd";
 
 import FiltersSort from "./FiltersSort";
+import FiltersTypes from "../todoForm/TodoTypes";
 
 const Filters = () => {
   return (
@@ -9,6 +10,9 @@ const Filters = () => {
         <Row justify="center" wrap={false} gutter={20}>
           <Col span={4}>
             <FiltersSort />
+          </Col>
+          <Col span={4}>
+            <FiltersTypes />
           </Col>
         </Row>
       </Form>
